834a49ab4d55564489f59ae35734e5c93dfea2c4
Potapenko <nylestroke@gmail.com>
2023-10-28 17:03:00 +0200

    Fix a bug in LayerStack which would occur if passing an overlay

--- Engine/Source/Runtime/Private/Core/LayerStack.cpp
+++ Engine/Source/Runtime/Private/Core/LayerStack.cpp
@@ -28,7 +28,7 @@ namespace Hyperion {
 	{
 		auto it = std::find(m_Layers.begin(), 
 			m_Layers.begin() + m_LayerInsertIndex, layer);
-		if (it != m_Layers.end())
+		if (it != m_Layers.begin() + m_LayerInsertIndex)
 		{
 			layer->OnDetach();
 			m_Layers.erase(it);
