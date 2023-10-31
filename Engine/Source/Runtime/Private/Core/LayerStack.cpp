a89ff76bd08c42099269a645eb23495f38f3e218
Potapenko <nylestroke@gmail.com>
2023-10-31 17:02:20 +0100

    Call the layer's OnDetach before deleting it

--- Engine/Source/Runtime/Private/Core/LayerStack.cpp
+++ Engine/Source/Runtime/Private/Core/LayerStack.cpp
@@ -7,7 +7,10 @@ namespace Hyperion {
 	LayerStack::~LayerStack()
 	{
 		for (Layer* layer : m_Layers)
+		{
+			layer->OnDetach();
 			delete layer;
+		}
 	}
 
 	void LayerStack::PushLayer(Layer* layer)
