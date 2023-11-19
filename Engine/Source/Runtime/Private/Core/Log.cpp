e28c15b0aeafc2ed4dc9dc5b49060741eb10845b
nylestroke@gmail.com <nylestroke@gmail.com>
2023-11-19 15:26:16 +0100

    Save log messages into a file Hyperion.log at Editor directory

--- Engine/Source/Runtime/Private/Core/Log.cpp
+++ Engine/Source/Runtime/Private/Core/Log.cpp
@@ -3,6 +3,7 @@
 #include "Runtime/Core/Log.hpp"
 
 #include <spdlog/sinks/stdout_color_sinks.h>
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Hyperion {
 
@@ -11,13 +12,22 @@ namespace Hyperion {
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^ [%T] %n: %v%$");
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Hyperion.log", true));
 
-		s_CoreLogger = spdlog::stdout_color_mt("RUNTIME");
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = std::make_shared<spdlog::logger>("RUNTIME", begin(logSinks), end(logSinks));
+		register_logger(s_CoreLogger);
 		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
 
-		s_ClientLogger = spdlog::stdout_color_mt("EDITOR");
+		s_ClientLogger = std::make_shared<spdlog::logger>("EDITOR", begin(logSinks), end(logSinks));
+		register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 
 }
