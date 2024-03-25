323c68dcb31423d19653a0cf9b7f911f004f9412
Vladyslav Potapenko <nylestroke@gmail.com>
2024-03-25 13:25:19 +0100

    Fix that Event::Handled could be set back to false

--- Engine/Hyperion-Core/Source/Public/Runtime/Events/Event.hpp
+++ Engine/Hyperion-Core/Source/Public/Runtime/Events/Event.hpp
@@ -73,7 +73,7 @@ namespace Hyperion {
 		{
 			if (m_Event.GetEventType() == T::GetStaticType())
 			{
-				m_Event.Handled = func(static_cast<T&>(m_Event));
+				m_Event.Handled |= func(static_cast<T&>(m_Event));
 				return true;
 			}
 			return false;
