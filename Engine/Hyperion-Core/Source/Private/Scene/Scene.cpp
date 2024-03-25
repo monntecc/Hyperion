e8af148e7d1ffb8259d6ff2b1ad31cc0d7977c88
Vladyslav Potapenko <nylestroke@gmail.com>
2024-03-25 13:08:36 +0100

    Removed redundant mainCamera->GetProjection() in Scene::OnUpdate

--- Engine/Hyperion-Core/Source/Private/Scene/Scene.cpp
+++ Engine/Hyperion-Core/Source/Private/Scene/Scene.cpp
@@ -65,7 +65,7 @@ namespace Hyperion {
 
 		if (mainCamera)
 		{
-			Renderer2D::BeginScene(mainCamera->GetProjection(), *cameraTransform);
+			Renderer2D::BeginScene(*mainCamera, *cameraTransform);
 
 			const auto group = m_Registry.group<TransformComponent>(entt::get<SpriteRendererComponent>);
 			for (const auto entity : group)
