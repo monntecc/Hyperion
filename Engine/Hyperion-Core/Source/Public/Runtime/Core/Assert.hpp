ba7180a8d29f60341fa06b23e834eb0c8f7a5591
Vladyslav Potapenko <nylestroke@gmail.com>
2024-03-25 13:19:26 +0100

    Replace filename extraction with equivalent standard library function

--- Engine/Hyperion-Core/Source/Public/Runtime/Core/Assert.hpp
+++ Engine/Hyperion-Core/Source/Public/Runtime/Core/Assert.hpp
@@ -3,35 +3,15 @@
 #include "Runtime/Core/Base.hpp"
 #include "Runtime/Core/Log.hpp"
 
+#include <filesystem>
+
 #ifdef HR_ENABLE_ASSERTS
-
-namespace Hyperion::Assert {
-
-	// Returns the simple file name rather than full path
-	constexpr const char* CurrentFileName(const char* path) {
-		const char* file = path;
-		while (*path)
-		{
-			if (*path == '/' || *path == '\\')
-			{
-				file = ++path;
-			}
-			else
-			{
-				path++;
-			}
-		}
-
-		return file;
-	}
-
-}
 
 // Alteratively we could use the same "default" message for both "WITH_MSG" and "NO_MSG" and
 	// provide support for custom formatting by concatenating the formatting string instead of having the format inside the default message
 #define HR_INTERNAL_ASSERT_IMPL(type, check, msg, ...) { if(!(check)) { HR##type##ERROR(msg, __VA_ARGS__); HR_DEBUGBREAK(); } }
 #define HR_INTERNAL_ASSERT_WITH_MSG(type, check, ...) HR_INTERNAL_ASSERT_IMPL(type, check, "Assertion failed: {0}", __VA_ARGS__)
-#define HR_INTERNAL_ASSERT_NO_MSG(type, check) HR_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", HR_STRINGIFY_MACRO(check), ::Hyperion::Assert::CurrentFileName(__FILE__), __LINE__)
+#define HR_INTERNAL_ASSERT_NO_MSG(type, check) HR_INTERNAL_ASSERT_IMPL(type, check, "Assertion '{0}' failed at {1}:{2}", HR_STRINGIFY_MACRO(check), std::filesystem::path(__FILE__).filename().string(), __LINE__)
 
 #define HR_INTERNAL_ASSERT_GET_MACRO_NAME(arg1, arg2, macro, ...) macro
 #define HR_INTERNAL_ASSERT_GET_MACRO(...) HR_EXPAND_MACRO( HR_INTERNAL_ASSERT_GET_MACRO_NAME(__VA_ARGS__, HR_INTERNAL_ASSERT_WITH_MSG, HR_INTERNAL_ASSERT_NO_MSG) )
